e047b3b02372ef252cc1be21dab1a0f69d18e539
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-09-08 16:58:25 +0530

    okieee

---
+++ script.js
@@ -0,0 +1,19 @@
+// // 4 Pillars of Dom 
+// // a.Selection Of Element in JS
+// var a = document.querySelector("h1");
+// console.log(a);
+
+// // b.Changing HTML
+// var a = document.querySelector("h1");
+// a.innerHTML="Hello";
+
+// // c.Changing CSS
+// var a = document.querySelector("h1");
+// a.style.color="blue"
+
+// // d.EventListner
+// var a = document.querySelector("h1");
+// a.addEventListener("click",function(){
+//     console.log("bye");
+// })
+
