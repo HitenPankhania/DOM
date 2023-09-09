aa268d92c1c5566e177e7ee9ebe95ea35f184408
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-09-09 21:55:18 +0530

    okieee

--- script.js
+++ script.js
@@ -20,15 +20,15 @@
 // var bulb = document.querySelector("#bulb");
 // var btn = document.querySelector("button");
 
-// var flag = 10;
+// var flag = 0;
 
 // btn.addEventListener("click",function(){
-//     if(flag==10){
+//     if(flag==0){
 //         bulb.style.backgroundColor = "yellow"
 //         flag=1
 //     }else{
 //          bulb.style.backgroundColor = "#ffff"
-//     flag=10
+//     flag=0
 
 //     }  
 // })
