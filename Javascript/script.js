028fe314597650c13959d6b777c73b82f95c8113
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-09-10 16:21:43 +0530

    okiee

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+
+//Constructor Function in javascript
+
+
+
+function saachaofBiscuit(){
+    this.width = 12;
+    this.height = 22;
+    this.color = "blue";
+    this.taste = "sugary";
+}
+
+
+var bis1 = new saachaofBiscuit();
+var bis2 = new saachaofBiscuit();
+var bis3 = new saachaofBiscuit();
+var bis4 = new saachaofBiscuit();
+var bis5 = new saachaofBiscuit();
+var bis6 = new saachaofBiscuit();
+var bis7 = new saachaofBiscuit();
+var bis8 = new saachaofBiscuit();
+var bis9 = new saachaofBiscuit();
+var bis10 = new saachaofBiscuit();
+var bis11 = new saachaofBiscuit();
+var bis12 = new saachaofBiscuit();
