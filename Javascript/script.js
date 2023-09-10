62ae4e5872165de4e96a04f72d08592756c837b3
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-09-10 16:42:41 +0530

    okieee

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,23 +3,40 @@
 
 
 
-function saachaofBiscuit(){
-    this.width = 12;
-    this.height = 22;
-    this.color = "blue";
-    this.taste = "sugary";
-}
+// function saachaofBiscuit(){
+//     this.width = 12;
+//     this.height = 22;
+//     this.color = "blue";
+//     this.taste = "sugary";
+// }
 
 
-var bis1 = new saachaofBiscuit();
-var bis2 = new saachaofBiscuit();
-var bis3 = new saachaofBiscuit();
-var bis4 = new saachaofBiscuit();
-var bis5 = new saachaofBiscuit();
-var bis6 = new saachaofBiscuit();
-var bis7 = new saachaofBiscuit();
-var bis8 = new saachaofBiscuit();
-var bis9 = new saachaofBiscuit();
-var bis10 = new saachaofBiscuit();
-var bis11 = new saachaofBiscuit();
-var bis12 = new saachaofBiscuit();
+// var bis1 = new saachaofBiscuit();
+// var bis2 = new saachaofBiscuit();
+// var bis3 = new saachaofBiscuit();
+// var bis4 = new saachaofBiscuit();
+// var bis5 = new saachaofBiscuit();
+// var bis6 = new saachaofBiscuit();
+// var bis7 = new saachaofBiscuit();
+// var bis8 = new saachaofBiscuit();
+// var bis9 = new saachaofBiscuit();
+// var bis10 = new saachaofBiscuit();
+// var bis11 = new saachaofBiscuit();
+// var bis12 = new saachaofBiscuit();
+
+
+
+
+
+
+//IIFE
+
+
+// // Creating Function Expression by assigning to a variable.
+// var myFunc = function() { return "GeeksforGeeks"; };
+  
+// // Creating Function Expression Using Logical Not.
+// !function() { return "GeeksforGeeks"; };
+  
+// // Creating Function Expression Using Parentheses.
+// (function() { return "GeeksforGeeks"; });
