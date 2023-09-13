1ba1bed26fe66b69c4dc39ee892ee32988a36e93
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-09-13 19:39:51 +0530

    okiee

---
+++ Javascript  A class/script.js
@@ -0,0 +1,130 @@
+// 1. Create a function and print 'This is Calculator'
+//  function printCalculator(){
+//     console.log("This is Calculator");
+// }
+// printCalculator();
+
+// 2. Create a function that should return a string 'This is calculator' and print it.
+//  function printHello (){
+//     const message = 'This is Calculator';
+//     console.log(message);
+//     return message;
+// }
+// printHello();
+
+
+// 3. Create a function that takes three parameters and print all those.
+// function printParameter(para1,para2,para3){
+//     console.log(para1,para2,para3)
+
+// }
+// printParameter();
+
+// 4. Create a function that takes three parameters as numbers and returns the sum of the three numbers.
+// function sumNumber(num1,num2,num3){
+//     // const sum = num1+num2+num3;
+//     return sumNumber;
+// }
+
+
+// 5. Create a function called 'sumWithValidation' that takes three parameters as numbers and returns the sum only if all of those parameters are numbers, otherwise return 0.
+//   function sumWithValidation(param1, param2, param3) {
+//     if (typeof param1 === 'number' && typeof param2 === 'number' && typeof param3 === 'number') {
+//       // Check if all three parameters are numbers
+//       return param1 + param2 + param3; // If they are, return their sum
+//     } else {
+//       return 0; // If any parameter is not a number, return 0
+//     }
+//   }
+
+  // 6. Create a function for multiplication of two numbers and return the value.
+//   function multiPly(num1, num2) {
+    // const result = num1 * num2; // Calculate the product of two numbers
+    return result; // Return the result
+  }
+  multiPly();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 1. Create a function and print 'This is Calculator'
+// function printCalculator() {
+//     console.log('This is Calculator');
+//   }
+  
+//   // 2. Create a function that should return a string 'This is calculator' and print it.
+//   function returnAndPrintCalculator() {
+//     const message = 'This is calculator'; // Create a message
+//     console.log(message); // Print the message
+//     return message; // Return the message
+//   }
+  
+//   // 3. Create a function that takes three parameters and print all those.
+//   function printParameters(param1, param2, param3) {
+//     console.log(param1, param2, param3); // Print the three parameters
+//   }
+  
+//   // 4. Create a function that takes three parameters as numbers and returns the sum of the three numbers.
+//   function sumNumbers(num1, num2, num3) {
+//     const sum = num1 + num2 + num3; // Calculate the sum of the three numbers
+//     return sum; // Return the result
+//   }
+  
+//   // 5. Create a function called 'sumWithValidation' that takes three parameters as numbers and returns the sum only if all of those parameters are numbers, otherwise return 0.
+//   function sumWithValidation(param1, param2, param3) {
+//     if (typeof param1 === 'number' && typeof param2 === 'number' && typeof param3 === 'number') {
+//       // Check if all three parameters are numbers
+//       return param1 + param2 + param3; // If they are, return their sum
+//     } else {
+//       return 0; // If any parameter is not a number, return 0
+//     }
+//   }
+  
+//   // 6. Create a function for multiplication of two numbers and return the value.
+//   function multiply(num1, num2) {
+//     const result = num1 * num2; // Calculate the product of two numbers
+//     return result; // Return the result
+//   }
+  
+//   // 7. Create a function using function expression and print 'This is Function expression'
+//   const functionExpression = function() {
+//     console.log('This is Function expression');
+//   };
+  
+//   // 8. Create a function using function expression that takes one parameter as input and returns it.
+//   const returnParameter = function(input) {
+//     return input; // Return the parameter as it is
+//   };
+  
+  // Export the functions to make them accessible in other JavaScript files
+//   module.exports = {
+//     printCalculator,
+//     returnAndPrintCalculator,
+//     printParameters,
+//     sumNumbers,
+//     sumWithValidation,
+//     multiply,
+//     functionExpression,
+//     returnParameter,
+//   };
+  
